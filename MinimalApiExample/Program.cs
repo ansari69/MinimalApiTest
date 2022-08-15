5037853a709944a338763943e1295bd3231ff87f
ansari69 <mohamad.ansariyan8@gmail.com>
2022-08-15 12:21:21 +0430

    Add end point test

--- MinimalApiExample/Program.cs
+++ MinimalApiExample/Program.cs
@@ -35,6 +35,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapGet("/", () => "Hello World!");
+
 app.Run();
 
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
