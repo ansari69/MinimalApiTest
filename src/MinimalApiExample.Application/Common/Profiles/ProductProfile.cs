f4bd8f7a84a52c6bdeed3573c24ee9e76bc700b1
ansari69 <mohamad.ansariyan8@gmail.com>
2022-08-19 01:16:38 +0430

    Create upsert product

---
+++ src/MinimalApiExample.Application/Common/Profiles/ProductProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MinimalApiExample.Application.Products.Commands.UpsertProduct;
+using MinimalApiExample.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalApiExample.Application.Common.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<UpsertProductCommand, Product>();
+        }
+    }
+}
---
+++ src/MinimalApiExample.Application/Products/Commands/UpsertProduct/UpsertProductCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MediatR;
+using MinimalApiExample.Application.Common.Interfaces;
+using MinimalApiExample.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalApiExample.Application.Products.Commands.UpsertProduct
+{
+    public class UpsertProductCommandHandler : IRequestHandler<UpsertProductCommand, string>
+    {
+        private readonly IAppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UpsertProductCommandHandler(IAppDbContext context, IMapper mapper, IMediator mediator)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<string> Handle(UpsertProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product();
+
+            if (request.ProductId == null)
+            {
+                //Insert
+                product.ProductId = Guid.NewGuid().ToString();
+                product.CreateDate = DateTime.Now;
+
+                _context.Products.Add(product);
+            }
+            else
+            {
+                product = await _context.Products.FindAsync(request.ProductId);
+             }
+
+            product = _mapper.Map<UpsertProductCommand, Product>(request, product);
+
+            await _context.SaveChangesAsync();
+
+            return product.ProductId;
+        }
+    }
+}
