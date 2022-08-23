a2a78a38792c0c256b090966bbd8f10d38b03513
ansari69 <mohamad.ansariyan8@gmail.com>
2022-08-23 12:07:50 +0430

    Validate product

---
+++ src/MinimalApiExample.Application/Products/Commands/ValidateProduct/ValidateProductCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalApiExample.Application.Products.Commands.ValidateProduct
+{
+    public class ValidateProductCommand : IRequest<bool>
+    {
+        public string ProductId { get; set; }
+
+    }
+}
